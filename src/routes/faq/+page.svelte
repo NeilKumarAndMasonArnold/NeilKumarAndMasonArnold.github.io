0f55de58345de63183ae66fe1e0119920c312b7a
Neil A. Kumar <denkrishna+github1@gmail.com>
2024-06-11 11:49:02 -0600

    FAQ Page
    Create the FAQ Page with a Question component for easy formatting.

---
+++ src/routes/faq/+page.svelte
@@ -0,0 +1,69 @@
+<script>
+	import Question from './Question.svelte';
+	import { fly } from 'svelte/transition';
+	import { quintOut } from 'svelte/easing';
+</script>
+
+<div class="wrapper" in:fly={{ y: 10, easing: quintOut, duration: 750 }}>
+	<h1>FAQ – Frequently Asked Questions</h1>
+
+	<Question>
+		<h4 slot="question"><span>Q: </span>Why might I see a swastika at/on [location]?</h4>
+		<p slot="answer">
+			<span>A: </span>As a Hindu and a Jew with elements of Christian syncretism, we are intimately
+			familiar with the cultural complexities that can arise when the same symbol carries different
+			meanings across cultures. The symbol you might encounter at certain ceremonies or on some
+			decor is the "swastik" from Hindu tradition. It signifies auspiciousness, prosperity, and good
+			fortune and has been a cherished symbol in Hindu rituals and art for thousands of years. We
+			are deeply aware that for many, especially from a Jewish background, the swastika not only
+			evokes painful memories due to its appropriation by the Nazis during World War II but also
+			resonates with the disturbing misappropriation by white supremacists and neo-Nazis in more
+			recent times. In respect for our diverse guest list, we've endeavored to minimize its display
+			during our celebrations. For a detailed history of the symbol, please
+			<a href="/about/history_of_the_swastik/swastika">click here</a>
+		</p>
+	</Question>
+
+	<Question>
+		<h4 slot="question"><span>Q: </span>Why is do I see a swastik covered / hidden?</h4>
+		<p slot="answer">
+			<span>A: </span>We recognize the deep sensitivity surrounding the swastika symbol, especially
+			given its association with the Holocaust. Out of respect for family members and guests, we've
+			minimized the usage of the swastik symbol during our ceremonies and decor. However, you might
+			still encounter it in a few traditional Hindu contexts. We appreciate everyone's understanding
+			and compassion as we merge two beautiful cultures. For a detailed history of the symbol,
+			please <a href="/about/history_of_the_swastik/swastika">click here</a>
+		</p>
+	</Question>
+
+	<Question>
+		<h4 slot="question"><span>Q: </span>Will any of the cermonies have a dress code?</h4>
+		<p slot="answer"><span>A: </span>Information on this will be coming soon!</p>
+	</Question>
+
+	<Question>
+		<h4 slot="question"><span>Q: </span>Will there be drinks?</h4>
+		<p slot="answer"><span>A: </span>Information on this will be coming soon!</p>
+	</Question>
+</div>
+
+<style>
+	.wrapper {
+		height: max-content;
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		gap: 2rem;
+	}
+
+	h4,
+	p {
+		text-align: left;
+		font-weight: var(--fw-normal);
+	}
+
+	span {
+		font-size: var(--fs-semi-large);
+		font-weight: var(--fw-bold);
+	}
+</style>
---
+++ src/routes/faq/Question.svelte
@@ -0,0 +1,104 @@
+<!--
+FAQ Question Component (Question.svelte)
+Renders a question-answer pair for the FAQ page with the answer as a drop-down.
+
+Author: Neil A. Kumar (2024)
+Status: Complete
+-->
+
+<script>
+	import { iconLibrary } from '$lib';
+	import { quintOut } from 'svelte/easing';
+	import { slide } from 'svelte/transition';
+	import Icon from '$lib/components/Icon.svelte';
+
+	let expanded = false;
+</script>
+
+<div class="question-container" class:expanded>
+	<div class="dropdown-container" class:closed={!expanded}>
+		<div
+			role="button"
+			tabindex="0"
+			aria-label="Open Question"
+			aria-expanded={expanded}
+			class="question"
+			class:closed={!expanded}
+			on:click={() => (expanded = !expanded)}
+			on:keypress={() => (expanded = !expanded)}
+		>
+			<h4><slot name="question" /></h4>
+			<div id="arrow" style:transform={expanded ? 'rotate(0deg)' : 'rotate(-180deg)'}>
+				<Icon
+					path={iconLibrary['dropdown'].path}
+					fill="none"
+					stroke="var(--c-text-nav)"
+					stroke-width="3px"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					viewBox={iconLibrary['dropdown'].viewBox}
+				/>
+			</div>
+		</div>
+	</div>
+
+	{#if expanded}
+		<div class="answer" transition:slide={{ easing: quintOut, duration: 500 }}>
+			<slot name="answer" />
+		</div>
+	{/if}
+</div>
+
+<style>
+	.question-container {
+		border: 1px solid var(--c-text-main);
+		border-radius: 20px;
+		overflow: hidden;
+	}
+
+	.question-container.expanded {
+		box-shadow: 0 9.3905074804px 15px var(--c-shadow-gallery);
+	}
+
+	.dropdown-container {
+		background-color: var(--c-background-nav);
+	}
+
+	.question {
+		align-items: center;
+		background-color: transparent;
+		border-bottom: 1px solid var(--c-text-main);
+		border-radius: 0;
+		cursor: pointer;
+		display: flex;
+		justify-content: space-between;
+		padding: 0.75rem 1rem;
+	}
+
+	.question:hover {
+		background-color: var(--c-background-hover);
+	}
+
+	.closed {
+		border-bottom: 0 solid var(--c-text-main);
+		border-radius: 20px;
+	}
+
+	#arrow {
+		height: var(--fs-large);
+		user-select: none;
+	}
+
+	.answer {
+		margin: 0.5rem;
+		justify-items: center;
+		gap: 0.5rem;
+		padding: 0.75rem 1rem;
+	}
+
+	@media (min-width: 767px) {
+		.answer {
+			gap: normal;
+		}
+	}
+</style>
