6da95f8a963c0f577767851079e78884be23e88b
Neil A. Kumar <denkrishna+github1@gmail.com>
2024-06-11 10:19:00 -0600

    LOL
    This is definitely a bad idea lol

---
+++ build/_app/immutable/nodes/9.Bk-tLxpl.js
@@ -0,0 +1,1 @@
+import{s}from"../chunks/scheduler.DQ6YJph1.js";import{S as t,i as e}from"../chunks/index.DfvAYoco.js";class l extends t{constructor(o){super(),e(this,o,null,null,s,{})}}export{l as component};
