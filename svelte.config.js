c0da7adfeb7418e56bcb41f85a07e2927e2ce71d
Neil A. Kumar <denkrishna+github1@gmail.com>
2024-06-11 09:41:14 -0600

    Troubleshooting GH Pages
    Looking to see if a new configuration fixes anything

--- svelte.config.js
+++ svelte.config.js
@@ -19,7 +19,7 @@ const config = {
 			strict: true
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			base: process.env.NODE_ENV === 'development' ? '' : process.env.BASE_PATH || ''
 		}
 	}
 };
