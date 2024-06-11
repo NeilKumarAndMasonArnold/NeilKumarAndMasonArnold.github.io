a42a97c1814e417b30e12825dc2924da23cf79eb
Neil A. Kumar <denkrishna+github1@gmail.com>
2024-06-11 11:31:44 -0600

    Comming Soon
    Added the comming soon text to currently undeveloped pages.

---
+++ src/routes/about/schedule/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { fly } from 'svelte/transition';
+	import { quintOut } from 'svelte/easing';
+</script>
+
+<div class="wrapper" in:fly={{ y: 10, easing: quintOut, duration: 750 }}>
+	<h1>Schedule</h1>
+	<p>Coming Soon!</p>
+</div>
