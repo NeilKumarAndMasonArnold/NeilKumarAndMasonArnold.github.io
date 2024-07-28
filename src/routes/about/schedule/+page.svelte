5156c0df81819e6e7f10484ad7cbe41fd84a8f26
Neil A. Kumar <142553221+NeilKumarAndMasonArnold@users.noreply.github.com>
2024-07-28 17:19:22 -0400

    Added schedule timeline
    Created a timeline component and a timeline event component to render the different events on the schedule page

--- src/routes/about/schedule/+page.svelte
+++ src/routes/about/schedule/+page.svelte
@@ -1,14 +1,60 @@
 <script>
 	import { fly } from 'svelte/transition';
 	import { quintOut } from 'svelte/easing';
+	import Timeline from './Timeline.svelte';
+
+	let events = [
+		// {
+		// 	title: 'Bachelor Party',
+		// 	date: 'TBD, 2025',
+		// 	description: 'lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+		// },
+		// {
+		// 	title: 'Bachelorette Party',
+		// 	date: 'TBD, 2025',
+		// 	description: 'lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+		// },
+		{
+			// title: 'Haldi Ceremony / Mikveh',
+			title: 'Haldi Ceremony',
+			date: 'TBD, 2025',
+			description: 'lorem ipsum dolor sit amet, consecteture adipiscing elit.'
+		},
+		{
+			title: 'Mehendi Ceremony',
+			date: 'TBD, 2025',
+			description:
+				"A mehendi ceremony is a pre-wedding ritual in many South Asian cultures, particularly in India.  Traditionally, it involves appying intricate henna designs to the bride's hands and feet.  Often held a day or two before the wedding, the ceremony is attended by a smaller cohort of close family and friends.  The mehendi is believed to bring good luck and acts as a symbol of joy and beauty."
+		},
+		{
+			title: 'Wedding Ceremony',
+			date: 'August 17th, 2025',
+			description: 'lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+		},
+		{
+			title: 'Sangeet / Reception',
+			date: 'TBD, 2025',
+			description: 'lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+		}
+	];
 </script>
 
-<div class="wrapper" in:fly={{ y: 10, easing: quintOut, duration: 750 }}>
-	<h1>Schedule</h1>
-	<p>Coming Soon!</p>
+<div class="wrapper">
+	<h1 in:fly={{ y: 25, duration: 400, easing: quintOut }}>Schedule</h1>
+
+	<Timeline {events} />
 </div>
 
 <style>
+	.wrapper {
+		display: flex;
+		width: 100%;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		gap: 1rem;
+	}
+
 	h1,
 	p {
 		color: var(--c-text-main);
